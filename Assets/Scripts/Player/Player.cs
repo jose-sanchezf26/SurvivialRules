48f59b37f074c951f4677b256f58f71b5df2c9cf
Jose Antonio <joseantonio.sanchezf26@gmail.com>
2024-03-13 15:54:11 +0100

    El jugador mira hacia la dirección a la que se dirige

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -148,15 +148,8 @@ public class Player : MonoBehaviour
         explore.SetActive(true);
     }
 
-    void Update()
+    private void Movement()
     {
-        ManualControl();
-        if (Time.time % 1 == 0)
-        {
-            TimePassage();
-        }
-        detector.DetectObjectsInVision();
-
         //Se asigna la velocidad del personaje a los componentes de movimiento
         explore.moveSpeed = speed;
         agent.speed = speed;
@@ -172,6 +165,29 @@ public class Player : MonoBehaviour
             animator.SetFloat("Movement", 0);
         }
 
+        // Calcula la dirección del movimiento
+        float direction = Mathf.Sign(currentPosition.x - lastPosition.x);
+        if (direction > 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        if (direction < 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+
         lastPosition = currentPosition;
+    }
+
+    void Update()
+    {
+        ManualControl();
+        if (Time.time % 1 == 0)
+        {
+            TimePassage();
+        }
+        detector.DetectObjectsInVision();
+
+        Movement();
     }
 }
