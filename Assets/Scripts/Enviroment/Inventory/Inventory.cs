4d01770f51042fe8688007932ca52f6827cb1018
Jose Antonio <joseantonio.sanchezf26@gmail.com>
2024-03-19 13:49:03 +0100

    Implementación de inventario y colector de objetos, stone como objeto de prueba

---
+++ Assets/Scripts/Enviroment/Inventory/Inventory.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<InventoryItem> inventory = new List<InventoryItem>();
+    private Dictionary<ItemData, InventoryItem> itemDictionary = new Dictionary<ItemData, InventoryItem>();
+
+
+    private void OnEnable()
+    {
+        Stone.OnStoneCollected += Add;
+    }
+
+    private void OnDisable()
+    {
+        Stone.OnStoneCollected -= Add;
+    }
+
+    public void Add(ItemData itemData)
+    {
+        if (itemDictionary.TryGetValue(itemData, out InventoryItem item))
+        {
+            item.AddToStack(1);
+            Debug.Log($"{item.itemData.displayName} total stack is now {item.stackSize}.");
+        }
+        else
+        {
+            InventoryItem newItem = new InventoryItem(itemData);
+            inventory.Add(newItem);
+            itemDictionary.Add(itemData, newItem);
+            Debug.Log($"Added {newItem.itemData.displayName} to the inventory for the first time.");
+        }
+    }
+
+    public void Remove(ItemData itemData)
+    {
+        if (itemDictionary.TryGetValue(itemData, out InventoryItem item))
+        {
+            item.RemoveFromStack(1);
+            if (item.stackSize == 0)
+            {
+                inventory.Remove(item);
+                itemDictionary.Remove(itemData);
+            }
+        }
+    }
+
+}
