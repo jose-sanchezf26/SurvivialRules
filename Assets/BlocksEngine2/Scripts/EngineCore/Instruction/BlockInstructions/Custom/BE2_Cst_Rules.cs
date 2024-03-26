84f0615d40359576d501e408136496bc07f589c6
Jose Antonio <joseantonio.sanchezf26@gmail.com>
2024-03-26 20:58:42 +0100

    Creación del bloque Rules

---
+++ Assets/BlocksEngine2/Scripts/EngineCore/Instruction/BlockInstructions/Custom/BE2_Cst_Rules.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MG_BlocksEngine2.Block.Instruction;
+using MG_BlocksEngine2.Block;
+
+
+public class BE2_Cst_Rules : BE2_InstructionBase, I_BE2_Instruction
+{
+    public new void Function()
+    {
+        ExecuteSection(0);
+    }
+}
